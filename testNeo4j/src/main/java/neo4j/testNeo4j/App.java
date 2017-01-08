2a98fe9d087ee12699e34e384a927611da97dbab
hyqLeonardo <hyq335335@163.com>
2017-01-08 21:14:51 +0800

    first commit

---
+++ testNeo4j/src/main/java/neo4j/testNeo4j/App.java
@@ -0,0 +1,13 @@
+package neo4j.testNeo4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ testNeo4j/src/main/java/neo4j/testNeo4j/connection.java
@@ -0,0 +1,43 @@
+package neo4j.testNeo4j;
+
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Transaction;
+
+import static org.neo4j.driver.v1.Values.parameters;
+
+public class connection {
+	public static void main(String agrs[]) {
+		Driver driver = GraphDatabase.driver( "bolt://localhost:7687", AuthTokens.basic( "test", "123") );
+
+		try ( Session session = driver.session() )
+		{
+
+		    try ( Transaction tx = session.beginTransaction() )
+		    {
+		        tx.run( "CREATE (a:Person {name: {name}, title: {title}})",
+		                parameters( "name", "Arthur", "title", "King" ) );
+		        tx.success();
+		    }
+
+		    try ( Transaction tx = session.beginTransaction() )
+		    {
+		        StatementResult result = tx.run( "MATCH (a:Person) WHERE a.name = {name} " +
+		                                         "RETURN a.name AS name, a.title AS title",
+		                                         parameters( "name", "Arthur" ) );
+		        while ( result.hasNext() )
+		        {
+		            Record record = result.next();
+		            System.out.println( String.format( "%s %s", record.get( "title" ).asString(), record.get( "name" ).asString() ) );
+		        }
+		    }
+
+		}
+
+		driver.close();
+	}
+}
